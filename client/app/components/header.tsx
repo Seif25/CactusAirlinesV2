8910e0087e08af5dea88251c8f8abbf1cf0755b7
Seif <seif.abdelsalam@student.guc.edu.eg>
2023-08-16 18:20:28 +0300

    Login form & mobile-first

--- client/app/components/header.tsx
+++ client/app/components/header.tsx
@@ -3,28 +3,41 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useScrollPosition } from "../hooks/useScrollPosition";
+import { CiMenuBurger } from "react-icons/ci";
 
 const Header = () => {
   const scrollPosition = useScrollPosition()
   return (
-    <nav className={`flex items-center justify-between px-20 py-2 sticky top-0 z-50 ${scrollPosition > 0 ? "bg-[#FFEACE] shadow-sm" : "shadow-none bg-transparent"}`}>
-      <Link href={"/"} className="flex items-center gap-2">
-        <Image
-          src="/logo2.png"
-          alt="cactus airlines"
-          width={40}
-          height={40}
-          loading="lazy"
-        />
-        {/* <h1 className="text-xl text-[#005B48]">{scrollPosition}</h1> */}
-      </Link>
-      <div className="flex items-center justify-evenly gap-10">
-        <Link href={"/"} className="hover:underline hover:text-[#005B48]">Home</Link>
-        <Link href={"/#browse"} className="hover:underline hover:text-[#005B48]">Browse Flights</Link>
-        <Link href={"/favorites"} className="hover:underline hover:text-[#005B48]">Favorites</Link>
-        <Link href={"/my-flights"} className="hover:underline hover:text-[#005B48]">Bookings</Link>
+    <nav className={`px-2 lg:px-10 py-2 sticky top-0 z-50 ${scrollPosition > 0 ? "bg-[#FFEACE] shadow-sm" : "shadow-none bg-transparent"} w-full`}>
+      <div className="flex items-center justify-between">
+        <div className="lg:hidden">
+          <button>
+            <CiMenuBurger size={40}/>
+          </button>
+        </div>
+        <div>
+          <Link href={"/"} className="flex items-center gap-2">
+            <Image
+              src="/logo2.png"
+              alt="cactus airlines"
+              width={40}
+              height={40}
+              loading="lazy"
+            />
+            {/* <h1 className="text-xl text-[#005B48]">{scrollPosition}</h1> */}
+          </Link>
+        </div>
+        <div className="hidden lg:flex items-center justify-evenly gap-10">
+          <Link href={"/"} className="hover:underline hover:text-[#005B48]">Home</Link>
+          <Link href={"/#browse"} className="hover:underline hover:text-[#005B48]">Browse Flights</Link>
+          <Link href={"/favorites"} className="hover:underline hover:text-[#005B48]">Favorites</Link>
+          <Link href={"/my-flights"} className="hover:underline hover:text-[#005B48]">Bookings</Link>
+        </div>
+        <div>
+          <Link href={"/authentication/login"} className="hover:text-[#005B48] hover:bg-[#005B48] hover:bg-opacity-10 p-2 rounded-md hidden lg:flex">{"Login | Sign-up"}</Link>
+          <Link href={"/authentication/login"} className="text-white bg-[#005B48] p-2 rounded-md lg:hidden flex">{"Login"}</Link>
+        </div>
       </div>
-      <Link href={"/login"} className="hover:text-[#005B48] hover:bg-[#005B48] hover:bg-opacity-10 p-2 rounded-md">{"Login | Sign-up"}</Link>
     </nav>
   );
 };
--- client/app/draft.tsx
+++ client/app/draft.tsx
@@ -1,25 +1,9 @@
-"use client";
-
-import Image from "next/image";
-
-import { MdFlightClass } from "react-icons/md";
-import { BsFillPersonFill } from "react-icons/bs";
-import { BiSolidPlaneAlt } from "react-icons/bi";
-import { FaPlane } from "react-icons/fa";
-import { FiSearch } from "react-icons/fi";
-import Select from "./components/select";
-import { useState } from "react";
-import moment from "moment";
-
-export default function Home() {
-  const [trip, setTrip] = useState("Round-trip");
-  const [person, setPerson] = useState("1 Person");
-  const [flightClass, setFlightClass] = useState("Economy");
+const Draft = () => {
   return (
-    <div className="flex flex-col gap-20 py-10 lg:p-16">
+    <div className="p-0 lg:p-16 pb-40">
       {/* Hero */}
-      <div className="flex flex-col lg:flex-row items-center gap-10 h-[330px]">
-        <div className="flex flex-col justify-center h-full">
+      <div className="flex flex-col lg:grid lg:grid-cols-2 gap-10 h-auto pb-20 md:pb-40">
+        <div className="flex flex-col justify-center h-auto">
           <div className="absolute p-5">
             <h1 className="text-6xl font-extrabold w-64 text-[#005B48]">
               PLANNING A TRIP?
@@ -32,7 +16,7 @@ export default function Home() {
               Book Now
             </button>
           </div>
-          <div className="relative -z-10 top-[20%]">
+          <div className="relative -z-10 lg:top-[38%]">
             <Image
               src={"/gradient.jpg"}
               alt="plane"
@@ -43,12 +27,16 @@ export default function Home() {
             />
           </div>
         </div>
+        <div className=""></div>
       </div>
       {/* Flight Search */}
-      <div className="p-5 lg:p-0">
-        <div id="browse" className="bg-[#FFEACE] shadow-sm shadow-[#534439] w-full h-auto rounded-md p-5">
-          <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-5 pb-5">
-            <div className="flex flex-col lg:flex-row lg:items-center gap-5 w-auto">
+      <div className="p-5 md:p-0 lg:p-0">
+        <div
+          id="browse"
+          className="bg-[#FFEACE] shadow-sm shadow-[#534439] w-auto h-auto rounded-md p-5 flex flex-col"
+        >
+          <div className="flex flex-col lg:flex-row items-center justify-between pb-5">
+            <div className="flex flex-col lg:flex-row lg:items-center items-start gap-2 w-auto">
               <Select
                 options={["Round-trip", "One-way"]}
                 icon={<BiSolidPlaneAlt className="text-md" />}
@@ -77,7 +65,7 @@ export default function Home() {
               </h3>
             </div>
           </div>
-          <div className="bg-[#534439] bg-opacity-10 rounded-md w-auto h-full lg:h-[70px] flex flex-col lg:flex-row lg:items-center justify-evenly gap-5 p-5 lg:px-2">
+          <div className="bg-[#534439] bg-opacity-10 rounded-md w-auto h-[70px] flex flex-col lg:flex-row items-center justify-evenly gap-5 px-2">
             <div className="flex flex-col lg:flex-row items-center justify-evenly gap-5">
               <input
                 type="text"
@@ -91,12 +79,12 @@ export default function Home() {
                 placeholder="Anywhere"
               />
             </div>
-            <div className="border-t-2 lg:border-t-0 lg:border-l-2 border-[#534439] pl-2 flex items-center justify-center gap-10">
+            <div className="border-l-2 border-[#534439] pl-2 flex items-center justify-center gap-10">
               <input
                 type="date"
                 name="from"
                 id="from-date"
-                className="w-auto lg:w-[150px] bg-transparent rounded-md p-2"
+                className="w-[150px] bg-transparent rounded-md p-2"
                 defaultValue={moment().format("D, MM dd")}
               />
               -
@@ -104,16 +92,13 @@ export default function Home() {
                 type="date"
                 name="to"
                 id="to-date"
-                className="w-auto lg:w-[150px] bg-transparent rounded-md p-2"
+                className="w-[150px] bg-transparent rounded-md p-2"
                 defaultValue={moment().format("D, MM dd")}
               />
             </div>
             <div>
-              <button className="hidden bg-[#005B48] h-10 w-10 lg:flex items-center justify-center rounded-md">
+              <button className="bg-[#005B48] h-10 w-10 flex items-center justify-center rounded-md">
                 <FiSearch className="text-white text-xl transition ease-in-out delay-75 duration-150 hover:scale-125" />
-              </button>
-              <button className="lg:hidden bg-[#005B48] text-white h-10 px-10 w-full flex items-center justify-center rounded-md gap-3">
-                Browse Flights <FiSearch className="text-white text-xl" />
               </button>
             </div>
           </div>
@@ -121,4 +106,6 @@ export default function Home() {
       </div>
     </div>
   );
-}
+};
+
+export default Draft;
